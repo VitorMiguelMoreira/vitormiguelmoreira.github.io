32f929fba0af7d6a0e74bc8ec9b1cc4ff6af54ce
Vitor Miguel Moreira <vitorm2moreira@gmail.com>
2026-03-26 14:15:52 -0300

    Feat: added better responsiviness to mobile dispositives, like cellphones and tablets

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,8 +1,14 @@
 portugues = document.querySelector("#mudaLingua");
+ingles = document.querySelector("#mudaLingua2")
 textoPt = document.querySelectorAll(".portuguese");
 textoEn = document.querySelectorAll(".english");
 
 portugues.addEventListener("click", () => {
+    textoPt.forEach(el => el.classList.toggle('hide'))
+    textoEn.forEach(el => el.classList.toggle('hide'))
+})
+
+ingles.addEventListener("click", () => {
     textoPt.forEach(el => el.classList.toggle('hide'))
     textoEn.forEach(el => el.classList.toggle('hide'))
 })
